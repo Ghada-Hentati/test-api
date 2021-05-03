5b794fae8a9e7f5186f24c41f2d0670a344dfba7
projet pfa <ghadahacib2@gmail.com>
2021-05-03 13:37:24 +0100

    initial

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    jwtSecret: 'maskup-secret-key',
+    db: 'mongodb+srv://ghadanawel:maskup@maskup.eobya.mongodb.net/ionic-jwt?retryWrites=true&w=majority'
+    //db: 'mongodb://localhost/ionic-jwt'
+    //db: 'mongodb://192.168.0.5:9999/foo'
+};
+//username: ghadanawel
+//password: maskup
